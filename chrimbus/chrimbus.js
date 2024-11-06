c427371d7cf2ca8354a120b5c88b80b7e575beb4
ausnips <austinwmille@gmail.com>
2024-11-06 13:08:30 -0500

    updated chrimbus

--- chrimbus/chrimbus.js
+++ chrimbus/chrimbus.js
@@ -9,3 +9,15 @@ const createSnowflake = () => {
     setTimeout(() => snowflake.remove(), 5000);
 };
 setInterval(createSnowflake, 200);
+function playAllVideos() {
+    // Get all iframes in the video section
+    const iframes = document.querySelectorAll(".video-container iframe");
+    
+    // Set autoplay for each video by modifying the iframe src
+    iframes.forEach(iframe => {
+        const src = iframe.src;
+        if (!src.includes("autoplay=1")) {
+            iframe.src = src.includes("?") ? `${src}&autoplay=1` : `${src}?autoplay=1`;
+        }
+    });
+}
